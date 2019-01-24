2112c696cfb3472e80b8186ac3f006d4ec91775a
mitsunori.morita <mitsunori.morita@speee.jp>
2019-01-24 18:50:29 +0900

    props render change

--- src/components/books-section/books-section.js
+++ src/components/books-section/books-section.js
@@ -2,17 +2,29 @@ import React from 'react'
 import BooksContainer from './books-container';
 
 const BooksSection = (props) => {
-  return (
-    <section id='books-section'>
-    <div className='container'>
-      <div className='section-title'>
-        <h1>{props.title} Books</h1>
+  if(props.books.length === 0){
+    return (
+      <section id='books-section'>
+      <div className='container'>
+        <div className='section-title'>
+          <h1>No Books</h1>
+        </div>
       </div>
+    </section>
+    )
+  }else{
+    return (
+      <section id='books-section'>
+      <div className='container'>
+        <div className='section-title'>
+          <h1>{props.title} Books</h1>
+        </div>
 
-     <BooksContainer books={props.books}/>
-    </div>
-  </section>
-  )
+       <BooksContainer books={props.books}/>
+      </div>
+    </section>
+    )
+  }
 }
 
 export default BooksSection
